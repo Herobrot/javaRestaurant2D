8fe673c9d92877b3a2464e4f48097cbd3713d948
AlanAndreUp <cosaspami7@gmail.com>
2024-12-08 19:25:54 -0600

    hotfix

--- src/main/java/application/GameApp.java
+++ src/main/java/application/GameApp.java
@@ -4,21 +4,20 @@ import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import utils.GameConfig;
 import core.RestaurantGameEngine;
-import core.UIManager;
 import domain.models.CustomerStats;
 
 import static com.almasb.fxgl.dsl.FXGL.*;
 
 public class GameApp extends GameApplication {
     private RestaurantGameEngine gameEngine;
-    private UIManager uiManager;
+
     private CustomerStats customerStats;
 
     @Override
     protected void initSettings(GameSettings settings) {
         settings.setWidth(GameConfig.WINDOW_WIDTH);
         settings.setHeight(GameConfig.WINDOW_HEIGHT);
-        settings.setTitle("betooxx and art - BAR");
+        settings.setTitle("restaurante");
         settings.setVersion("777");
     }
 
@@ -33,7 +32,7 @@ public class GameApp extends GameApplication {
         gameEngine = new RestaurantGameEngine(getGameWorld(), customerStats);
 
         // Inicializar el administrador de UI
-        uiManager = new UIManager(customerStats);
+
 
         // Iniciar el juego
         setupGame();
@@ -45,9 +44,7 @@ public class GameApp extends GameApplication {
 
         // Inicializar componentes del juego
         gameEngine.initializeGameComponents();
-
-        // Configurar la UI
-        uiManager.initializeUI(getGameScene());
+        
 
         // Iniciar la generación de clientes
         gameEngine.startCustomerGeneration();
